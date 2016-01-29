31164ec7b9cd2a9bcfa7e7f031889b9b86118d41
neatonseatimes <neaton@seattletimes.com>
2016-01-28 17:04:45 -0800

    Published

--- src/js/main.js
+++ src/js/main.js
@@ -11,14 +11,14 @@ app.controller("uwController", ["$scope", function($scope) {
   $scope.recruits = huskiesData;
 
   $scope.headers = [
-    { title: "Name", short: "last" },
-    { title: "Position", short: "position" },
-    { title: "Rating", short: "rating" },
-    { title: "Height", short: "height" },
-    { title: "Weight", short: "weight" },
-    { title: "School", short: "school" },
-    { title: "Hometown", short: "hometown" },
-    { title: "Signed?", short: "signed" },
+    { title: "Name", short: "uwlastname" },
+    { title: "Position", short: "uwposition" },
+    { title: "Rating", short: "uwstars" },
+    { title: "Height", short: "uwheight" },
+    { title: "Weight", short: "uwweight" },
+    { title: "School", short: "uwschool" },
+    { title: "Hometown", short: "uwhometown" },
+    { title: "Signed?", short: "uwsigned" }
   ];
 
   $scope.selected = $scope.headers[0];
@@ -54,14 +54,14 @@ app.controller("wsuController", ["$scope", function($scope) {
   $scope.recruits = cougarsData;
 
   $scope.headers = [
-    { title: "Name", short: "last" },
-    { title: "Position", short: "position" },
-    { title: "Rating", short: "rating" },
-    { title: "Height", short: "height" },
-    { title: "Weight", short: "weight" },
-    { title: "School", short: "school" },
-    { title: "Hometown", short: "hometown" },
-    { title: "Signed?", short: "signed" },
+    { title: "Name", short: "wsulastname" },
+    { title: "Position", short: "wsuposition" },
+    { title: "Rating", short: "wsustars" },
+    { title: "Height", short: "wsuheight" },
+    { title: "Weight", short: "wsuweight" },
+    { title: "School", short: "wsuschool" },
+    { title: "Hometown", short: "wsuhometown" },
+    { title: "Signed?", short: "wsusigned" }
   ];
 
   $scope.selected = $scope.headers[0];
